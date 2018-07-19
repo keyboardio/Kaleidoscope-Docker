82caf38ff5602da07695377f7fafa8b2c80be74f
Gergely Nagy <algernon@keyboard.io>
2018-07-19 16:44:09 +0200

    entrypoint.sh: Always use kaleidoscope-builder, with a build argument
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -7,8 +7,4 @@ if [ -e /src/firmware/config/kaleidoscope-builder.conf ]; then
     cp /src/firmware/config/kaleidoscope-builder.conf .
 fi
 
-if [ -e Makefile ]; then
-    make $@
-else
-    /src/firmware/hardware/keyboardio/avr/libraries/Kaleidoscope/bin/kaleidoscope-builder $@
-fi
+/src/firmware/hardware/keyboardio/avr/libraries/Kaleidoscope/bin/kaleidoscope-builder build $@
