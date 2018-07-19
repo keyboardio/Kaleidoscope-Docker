03cfe1a8d39a1b6e76908a32433ea386cca2d9fe
Gergely Nagy <algernon@keyboard.io>
2018-07-19 22:50:13 +0200

    Better flashing support
    Instead of having to pipe the output of the builder into the flasher, rely on
    the `output/latest` symlink. For tools, instead of requiring the end-user to
    have them installed, copy them into `output/tools` from the container, and make
    the flasher use them.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -3,10 +3,11 @@ set -e
 
 cd /src/firmware/sketch
 
-if [ -e /src/firmware/config/kaleidoscope-builder.conf ]; then
-    cp /src/firmware/config/kaleidoscope-builder.conf .
-fi
+cp /src/firmware/config/kaleidoscope-builder.conf .
 
 /src/firmware/hardware/keyboardio/avr/libraries/Kaleidoscope/bin/kaleidoscope-builder build $@
+
+install -d /src/firmware/output/tools
+install /opt/teensy_loader_cli /usr/bin/avrdude /src/firmware/output/tools
 
 chown -R "${OWNER}" /src/firmware/output
