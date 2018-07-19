1037c47f815c45964483df8675fd928a250a32d7
Gergely Nagy <algernon@keyboard.io>
2018-07-19 06:57:14 +0200

    builder: Better source preparation
    Instead of mounting the source as-is, and perparing it within the container, do
    the preparation outside. This allows us to retain much more about it.
    
    With the new setup, we always try to clone first, and fall back to rsync if we
    can't.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -1,14 +1,7 @@
 #! /bin/sh
 set -e
 
-cp -r /src/firmware/src/ /src/firmware/sketch
-rm -rf /src/firmware/sketch/.git
-
 cd /src/firmware/sketch
-git init --quiet
-git config user.name Docker
-git config user.email docker@example.com
-git commit --quiet --allow-empty -m init
 
 if [ -e /src/firmware/config/kaleidoscope-builder.conf ]; then
     cp /src/firmware/config/kaleidoscope-builder.conf .
