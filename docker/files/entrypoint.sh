7d5d7f1c348331adec9653d89a93f047405b786d
Gergely Nagy <algernon@keyboard.io>
2018-07-25 22:53:26 +0200

    Use the avrdude from Arduino
    The avrdude binary that ships with Arduino has much less run-time
    dependencies (they are likely statically linked into it). Copy that outside
    instead of the one in the Debian package.
    
    Fixes #4.
    
    Signed-off-by: Gergely Nagy <algernon@keyboard.io>

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -8,6 +8,8 @@ cp /src/firmware/config/kaleidoscope-builder.conf .
 /src/firmware/hardware/keyboardio/avr/libraries/Kaleidoscope/bin/kaleidoscope-builder build $@
 
 install -d /src/firmware/output/tools
-install /opt/teensy_loader_cli /usr/bin/avrdude /src/firmware/output/tools
+install /opt/teensy_loader_cli  \
+        /opt/arduino-1.8.5/hardware/tools/avr/bin/avrdude \
+        /src/firmware/output/tools
 
 chown -R "${OWNER}" /src/firmware/output
